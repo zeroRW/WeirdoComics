56fb9450910b709b3b96bd44120066f25647e5de
AlanRLA <lopezz.alan134@gmail.com>
2022-12-07 23:05:47 -0600

    Vista de soliPedido (levantamiento chooser)
    están los btn pero remplazar por una img en el card

--- resources/views/soliPedido.blade.php
+++ resources/views/soliPedido.blade.php
@@ -6,7 +6,7 @@
 <br>
 <div class="container mt-3">
 
-    
+    {{-- Este se podria borrar --}}
         <div class="row gx-4">
           <div class="col">
            <div class="p-3 border bg-light text-center d-grid gap-2 col-6 mx-auto">
@@ -19,6 +19,22 @@
            </div>
           </div>
         </div>
+
+    {{-- Se quedaria este con la función que hacen los botones y agregando un img facha--}}
+        <div class="card-group">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Comic</h5>
+            </div>
+          </div>
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Articulo</h5>
+            </div>
+          </div>
+        </div> 
 
 
 </div>
